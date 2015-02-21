507db50f96423d8bda64fd3b53b6562775518a76
ernie <b01505025@ntu.edu.tw>
2015-02-21 15:53:21 +0800

    Range check remain float type, and rudientary error check haven't finished

--- forwithoutandor.c
+++ forwithoutandor.c
@@ -2,7 +2,37 @@
  *	A loop equivalent to the for without using && and ||.
  */
 #include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
+void printline(char line[], int length);
 
 main(){
+	char line[MAXLINE];
+	int length;
 
+	while((length = getline(line, MAXLINE)) > 0){
+		printline(line, length);
+	}
+}
+
+/* Read a line of character and return its length */
+int getline(char line[], int maxline){
+	int i, c;
+	for(i = 0; i < maxline; ++i){
+		if((c = getchar()) == '\n')break;
+		if(c == EOF)break;
+		line[i] = c;
+	}
+	if(c == '\n')line[i++] = c;
+	c = '\0';
+	return i;
+}
+
+/* Print a line of character */
+void printline(char line[], int length){
+	int i;
+	for(i = 0; i < length; ++i){
+		putchar(line[i]);
+	}
 }
--- rangeofcharshortintlong.c
+++ rangeofcharshortintlong.c
@@ -35,6 +35,58 @@ main(){
 	printf("\tlower limit: %c\n", d);
 
 	short s, t;
+	for(s = 0, t = s;; s++){
+		if(s < t)
+			break;
+		t = s;
+	}
+	printf("short\tupper limit: %d\n", t);
+	for(s = 0, t = s;; s--){
+		if(s > t)
+			break;
+		t = s;
+	}
+	printf("\tlower limit: %d\n", t);
 
 	long l, m;
+	for(l = 0, m = l;; l++){
+		if(l < m)
+			break;
+		m = l;
+	}
+	printf("long\tupper limit: %d\n", m);
+	for(l = 0, m = l;; l--){
+		if(l > m)
+			break;
+		m = l;
+	}
+	printf("\tloewr limit: %d\n", m);
+
+	signed g, h;
+	for(g = 0, h = g;; ++g){
+		if(g < h)
+			break;
+		h = g;
+	}
+	printf("signed\tupper limit: %d\n", g);
+	for(g = 0, h = g;; --g){
+		if(g > h)
+			break;
+		h = g;
+	}
+	printf("\tlower limit: %d\n", g);
+
+	unsigned u, v;
+	for(u = 0, v = u;; ++u){
+		if(u < v)
+			break;
+		u = v;
+	}
+	printf("unsignedupper limit: %d\n", v);
+	for(u = 0, v = u;; --u){
+		if(u > v)
+			break;
+		u = v;
+	}
+	printf("\tlower limit: %d\n", v);
 }
