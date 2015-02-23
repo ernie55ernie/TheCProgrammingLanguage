34eb7ee282f624415309aab11b59599e08c651c3
ernie <b01505025@ntu.edu.tw>
2015-02-23 18:33:16 +0800

    Rudimentary error check only the count of the symbols but ignore the subtle influence of symbol inside another symbol

--- rudimentaryerrorcheck.c
+++ rudimentaryerrorcheck.c
@@ -10,11 +10,32 @@ main(){
 	FILE *f;
 	int c;
 
+	int parentheses = 0;
+	int brackets = 0;
+	int braces = 0;
+	int singlequotes = 0;
+	int doublequotes = 0;
+
 	/* Get characters from file */
 	f = freopen("syntaxcheck.c", "r", stdin);
 	while((c = getchar()) != EOF){
+		if(c == '(') parentheses++;
+		if(c == ')') parentheses--;
+		if(c == '[') brackets++;
+		if(c == ']') brackets--;
+		if(c == '{') braces++;
+		if(c == '}') braces--;
+		if(c == '\"') singlequotes++;
+		if(c == '"') doublequotes++;
 		putchar(c);
 	}
 
+	printf("\n\nparentheses: %d\nbrackets: %d\nbraces: %d\nsinglequotes: %d\ndoublequotes: %d\n", parentheses, 
+		brackets, braces, singlequotes, doublequotes);
+	if(!parentheses && !brackets && !braces && 
+		singlequotes % 2 == 0 &&
+		doublequotes % 2 == 0)printf("No error with the count of symbol");
+
 	fclose(f);
 }
+
